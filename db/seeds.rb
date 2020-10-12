802b6b556f247630fb0c3326d4996fb46b57655d
0-jam <28038981+0-jam@users.noreply.github.com>
2020-10-12 16:15:00 +0900

    Added the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ['Japan', 'Japanese', 'Tokyo, Japan', 'Tokyo']
+
+names.each do |name|
+  Product.create!(name: name)
+end
